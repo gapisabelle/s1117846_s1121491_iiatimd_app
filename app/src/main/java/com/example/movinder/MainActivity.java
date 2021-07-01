8032b12f9ec3e7a7a3043659b7fc361beb7bb22e
gapisabelle <57439367+gapisabelle@users.noreply.github.com>
2021-07-01 22:03:48 +0200

    connectie naar apiiii

--- app/src/main/java/com/example/movinder/MainActivity.java
+++ app/src/main/java/com/example/movinder/MainActivity.java
@@ -4,24 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setUpFirebase();
-        Intent intent = new Intent(this, ChatActivity2.class);
-        startActivity(intent);
+        Button toRegister = findViewById(R.id.toRegister);
+        toRegister.setOnClickListener(this);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
     }
 
     public void setUpFirebase(){
         FirebaseOptions fbOptions = new FirebaseOptions.Builder()
                 .setProjectId("iiatimd-roy-isabelle").setApplicationId("1:948383510167:web:3f1aebc24b2f621e3a03d2").setApiKey("AIzaSyBSLidGsDuVHu8Poi5kQ_D_bhHm_fK48T8").setDatabaseUrl("https://iiatimd-roy-isabelle-default-rtdb.europe-west1.firebasedatabase.app").build();
         FirebaseApp.initializeApp(this, fbOptions, "Movinder");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent toRegister = new Intent(this, RegisterActivity.class);
+        startActivity(toRegister);
     }
 }
--- app/src/main/java/com/example/movinder/RegisterActivity.java
+++ app/src/main/java/com/example/movinder/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.example.movinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView toLogin = findViewById(R.id.toLogin);
+        toLogin.setOnClickListener(RegisterActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent toLogin = new Intent(this, MainActivity.class);
+        startActivity(toLogin);
     }
 }
