83c43add3c9bfeb7303f9d83fcbe90503980df90
Roy Oosterlee <roy@famoosterlee.nl>
2021-07-02 17:10:16 +0200

    Added Login & Register

--- app/src/main/java/com/example/movinder/ApiCallback.java
+++ app/src/main/java/com/example/movinder/ApiCallback.java
@@ -1,8 +1,18 @@
 package com.example.movinder;
 
+import org.json.JSONObject;
+
 public interface ApiCallback {
 
     default public void onCards(Card[] cards) {
+
+    }
+
+    default public void onRegister(JSONObject result) {
+
+    }
+
+    default public void onLogin(JSONObject result) {
 
     }
 }
